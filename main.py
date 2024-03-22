76195c1276ba18fef971d7ac530d920eecdff501
Adrián García Vázquez <adriangvazquez@gmail.com>
2024-03-22 19:03:00 +0100

    Update files

--- main.py
+++ main.py
@@ -1,14 +1,41 @@
-from typing import Optional
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+from sqlalchemy import create_engine
+from sqlalchemy.orm import registry, Session
+from fastapi import Depends, HTTPException
+
+engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
+session = Session(engine)
+registry().metadata.create_all(engine)
+
+# declarative base class
+Base = declarative_base()
+
+# an example mapping using the base
+class User(Base):
+    __tablename__ = "user"
+   
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    boolean = Column(Boolean, default=False)
+   
+
+
 
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello SwimChrono"}
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/")
+async def get_all_todos():
+    user_query = session.query(User)
+    return user_query.all()
+
+
+
+
+
