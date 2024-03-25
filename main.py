b11cb672c8e6150340ba9ee4d0c6dd0ec02834ef
Adrián García Vázquez <adriangvazquez@gmail.com>
2024-03-25 10:47:47 +0100

    Update main

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
 from sqlalchemy import create_engine
-from sqlalchemy.orm import registry, Session
-from fastapi import Depends, HTTPException
-
-engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
-session = Session(engine)
-registry().metadata.create_all(engine)
+from sqlalchemy.orm import Session
+from fastapi import FastAPI
 
 # declarative base class
 Base = declarative_base()
@@ -20,22 +14,22 @@ class User(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String)
     boolean = Column(Boolean, default=False)
-   
 
 
-
-from fastapi import FastAPI
+engine = create_engine("sqlite:///swimchrono.db", echo=True, future=True)
+Base.metadata.create_all(engine)
+session = Session(engine)
 
 app = FastAPI()
 
-
-
 @app.get("/")
-async def get_all_todos():
+async def get_all_users():
     user_query = session.query(User)
     return user_query.all()
 
-
-
-
-
+@app.post("/create")
+async def create_user(name: str, boolean: bool):
+    user = User(name=name, boolean=boolean)
+    session.add(user)
+    session.commit()
+    return {"user added": user.id}
